b019888b73c3f6ef0256dcecb3b996e2461534f3
CarterK33 <carter33Kindred@gmail.com>
2023-05-31 12:19:17 -0500

    fixed update_hand

--- MIT_PS3.py
+++ MIT_PS3.py
@@ -81,31 +81,6 @@ def get_word_score(word, n):
     return sum_of_letters * second_component
     
     
-    """
-    Returns the score for a word. Assumes the word is a
-    valid word.
-
-    You may assume that the input word is always either a string of letters, 
-    or the empty string "". You may not assume that the string will only contain 
-    lowercase letters, so you will have to handle uppercase and mixed case strings 
-    appropriately. 
-
-	The score for a word is the product of two components:
-
-	The first component is the sum of the points for letters in the word.
-	The second component is the larger of:
-            1, or
-            7*wordlen - 3*(n-wordlen), where wordlen is the length of the word
-            and n is the hand length when the word was played
-
-	Letters are scored as in Scrabble; A is worth 1, B is
-	worth 3, C is worth 3, D is worth 2, E is worth 1, and so on.
-
-    word: string
-    n: int >= 0
-    returns: int >= 0
-    """
-    
     pass  # TO DO... Remove this line when you implement this function
 
 #
@@ -164,6 +139,19 @@ def deal_hand(n):
 # Problem #2: Update a hand by removing letters
 #
 def update_hand(hand, word):
+    
+    new_hand = hand.copy() 
+
+    for char in word: 
+        if char in new_hand.keys():
+            new_hand[char]-=1 
+        if new_hand[char] == 0:  
+            del new_hand[char] 
+
+    return new_hand
+
+    
+    
     """
     Does NOT assume that hand contains every letter in word at least as
     many times as the letter appears in word. Letters in word that don't
